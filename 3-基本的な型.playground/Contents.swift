aa894518e27edc1ffbcb303132d4cd41c2910a9f
Kai Aoyama <sauna.kaikai@gmail.com>
2021-06-11 16:04:20 +0900

    Any型、Tuple型

--- 3-基本的な型.playground/Contents.swift
+++ 3-基本的な型.playground/Contents.swift
@@ -1,5 +1,5 @@
-//import UIKit
-//import Foundation
+import UIKit
+import Foundation
 ////数値型に関してーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 //
 ////様々なビット数の整数型が表すことのできる最大値
@@ -37,75 +37,93 @@
 //
 //"abcde".range(of: "de")
 
+//
+////Optional型についてーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//
+//let none = Optional<Int>.none
+//print(".none: \(String(describing: none))")
+//
+//let some1 = Optional<String>.some("サンプル")
+//print(".some: \(String(describing: some1))")
+//print(some1)
+//
+////型推論で、プレースホルダ型の中身が、自動的に判別される。
+//let some2 = Optional.some(10)
+//let some3 = Optional.some("サンプルです")
+//
+////Optional型の値の生成
+//var optional:Int?
+//optional = nil  //nilリテラルを代入して、.noneを作成
+//optional = Optional(10)  //イニシャライザによる、.someの作成
+//optional = 120  //値の代入による、.someの作成
+//type(of: optional)
+//
+//
+////Optional型は、値を持っていない可能性があるため,Int？型のデータは四則演算ができない
+//var number1:Int? = 10
+//var number2:Int? = 20
+//var string:String
+//
+////number1 + number2  //普通にやると、エラーが発生する。
+//
+////強制アンラップ
+//number1! + number2!  //エラーがコンパイル時に発生しなくなるので、使い勝手はいいが、実行時にエラーが生じる可能性もあるので、使用には注意すること。
+//
+////オプショナルバインディング
+////if文を用いて、オプショナル型のの中身が存在していることを保証できている状態を作ると、処理を行うことができる。
+//if let optionalNumber1 = number1, let optionalNumber2 = number2 {
+//    optionalNumber1 + optionalNumber2
+//}
+//
+//// ??演算子を用いて、Optional型に値が存在しない場合のデフォルト値を設定する。
+//let optionalInt:Int? = nil
+//let Int = optionalInt ?? 100  //optionalIntの値がない場合は、デフォルト値である100が代入される。
+//
+////オプショナルチェイン ・・・アンラップをせずに、Wrapped型のメンバーにアクセスする方法
+//
+//let optionalDouble = Optional(1.0)
+//let optionalIsInfinite = optionalDouble?.isInfinite //オプショナル型のデータに「?」をつけることで、Wrapped型のメンバーを使用することができるようになる。
+//
+////map(_:)メソッドと、flatmap(_:)メソッド
+//
+////mapメソッド
+//let a = Optional(20)
+//let b = a.map { value in value * 2}
+//print(b)
+//type(of: b)
+//
+//let c = a.map { value in String(value) }
+//print(c)
+//type(of: c)
+//
+//
+////暗黙的なアンラップをしたものの取扱
+//
+//let k:Int? = 10 //ここでのkは、optional型で値が代入されているため、wrapped型のデータを使用したい時は下記のように強制的にアンラップしたり、オプショナルバインディングをしないと、使用することができない。
+//
+//k! + k!
+//
+//let j:Int! = 20 //こちらでは、暗黙的に値のアンラップをしているためそのまま値を使用することができるが、値が入っていない場合はプログラムの実行時にエラーが生じるため、あまり使わない方が吉
+//
+//j + j
 
-//Optional型についてーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
-let none = Optional<Int>.none
-print(".none: \(String(describing: none))")
+// Any型について ---------------------------------------------------------------------
 
-let some1 = Optional<String>.some("サンプル")
-print(".some: \(String(describing: some1))")
-print(some1)
+//型固有のメソッドやプロパティを使用することができなくなってしまうため、注意が必要
+let a :Any = 10
+let b :Any = 20
 
-//型推論で、プレースホルダ型の中身が、自動的に判別される。
-let some2 = Optional.some(10)
-let some3 = Optional.some("サンプルです")
+//タプル型について -------------------------------------------------------------------------
 
-//Optional型の値の生成
-var optional:Int?
-optional = nil  //nilリテラルを代入して、.noneを作成
-optional = Optional(10)  //イニシャライザによる、.someの作成
-optional = 120  //値の代入による、.someの作成
-type(of: optional)
+//複数の型をまとめて一つにまとめることができる。
+
+let tuple :(Int, String, Double?)
+
+tuple = (10, "テスト", 1.0)
+print(tuple)
 
 
-//Optional型は、値を持っていない可能性があるため,Int？型のデータは四則演算ができない
-var number1:Int? = 10
-var number2:Int? = 20
-var string:String
-
-//number1 + number2  //普通にやると、エラーが発生する。
-
-//強制アンラップ
-number1! + number2!  //エラーがコンパイル時に発生しなくなるので、使い勝手はいいが、実行時にエラーが生じる可能性もあるので、使用には注意すること。
-
-//オプショナルバインディング
-//if文を用いて、オプショナル型のの中身が存在していることを保証できている状態を作ると、処理を行うことができる。
-if let optionalNumber1 = number1, let optionalNumber2 = number2 {
-    optionalNumber1 + optionalNumber2
-}
-
-// ??演算子を用いて、Optional型に値が存在しない場合のデフォルト値を設定する。
-let optionalInt:Int? = nil
-let Int = optionalInt ?? 100  //optionalIntの値がない場合は、デフォルト値である100が代入される。
-
-//オプショナルチェイン ・・・アンラップをせずに、Wrapped型のメンバーにアクセスする方法
-
-let optionalDouble = Optional(1.0)
-let optionalIsInfinite = optionalDouble?.isInfinite //オプショナル型のデータに「?」をつけることで、Wrapped型のメンバーを使用することができるようになる。
-
-//map(_:)メソッドと、flatmap(_:)メソッド
-
-//mapメソッド
-let a = Optional(20)
-let b = a.map { value in value * 2}
-print(b)
-type(of: b)
-
-let c = a.map { value in String(value) }
-print(c)
-type(of: c)
-
-
-//暗黙的なアンラップをしたものの取扱
-
-let k:Int? = 10 //ここでのkは、optional型で値が代入されているため、wrapped型のデータを使用したい時は下記のように強制的にアンラップしたり、オプショナルバインディングをしないと、使用することができない。
-
-k! + k!
-
-let j:Int! = 20 //こちらでは、暗黙的に値のアンラップをしているためそのまま値を使用することができるが、値が入っていない場合はプログラムの実行時にエラーが生じるため、あまり使わない方が吉
-
-j + j
 
 
 
