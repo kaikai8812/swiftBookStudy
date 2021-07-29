c17a694b5b10b832356e3fd1b695e403dad6ee2c
Kai Aoyama <sauna.kaikai@gmail.com>
2021-07-30 01:56:15 +0900

    インスタンスプロパティとスタティックプロパティ

--- 7-型の構成要素.playground/Contents.swift
+++ 7-型の構成要素.playground/Contents.swift
@@ -31,3 +31,36 @@ struct SomeStruct1 {
 let some1 = SomeStruct1()
 some1.printSharedValue()
 
+// 7.3 プロパティについて 型に紐づいた値 ーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+//プロパティの種類 インスタンスプロパティと、スタティックプロパティ
+
+//インスタンスプロパティ  =>  型のインスタンスに基づくプロパティ
+//特に何も記述せずにプロパティを作成した場合は、このインスタンスプロパティになる
+
+//スタティックプロパティ => 型自身に紐づくプロパティ
+//staticと定数を定義する前に記述することで、型自身に紐づいた定数を作成することができる。
+//スタティックプロパティは、宣言時に最初から値を決めておく必要がある。
+
+struct Greeding{
+    var name:String = "太郎" //"太郎"という初期値を持ったインスタンスプロパティを作成
+    static let language = "English" //スタティックプロパティを作成
+    
+    func helloFunc(){
+        print("\(self.name)さんは、\(Self.language)を使用します。")  //selfで、インスタンスプロパティを使用、Selfで、スタティックプロパティを使用
+    }
+}
+
+var greet1 = Greeding()
+var greet2 = Greeding()
+
+greet2.name = "二郎"  //インスタンスプロパティの値を変更
+
+//プロパティが、greet1,greet2それぞれに紐づいたインスタンスプロパティなので、下記は値が異なっていることがわかる
+greet1.name //太郎
+greet2.name //二郎
+
+greet1.helloFunc()
+greet2.helloFunc()
+print(Greeding.language)  //型自身.スタティックプロパティ　で、アクセスすることができる。
+
